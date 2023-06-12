c3f5dddae71cb91252885a8117fcb8371f4f348a
JoseColmenares <jose08.col@gmail.com>
2023-06-12 16:11:11 -0500

    cambios2

---
+++ TestAuto/TestAuto/test/LoginSuccess.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'Validar los datos del Login ': function (browser) {
+        browser
+            .url('https://www.saucedemo.com/')
+            .waitForElementVisible('body', 1000)
+            .assert.titleContains('Swag Labs')
+            .assert.visible('#user-name')
+            .assert.visible('#login-button')
+            .setValue('#user-name', process.env.STANDARD)
+            .setValue('#password', process.env.PASSWORD)
+            .click('#login-button')
+            .waitForElementVisible('.app_logo')
+            .assert.visible('.app_logo')
+            .end();
+    },
+}
