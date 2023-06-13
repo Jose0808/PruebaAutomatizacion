137db85bc9e48dae9c40abda4f2ebbc171971d7b
JoseColmenares <jose08.col@gmail.com>
2023-06-13 11:34:47 -0500

    Cambios +6

--- PruebaTecnica-app/vite.config.js
+++ PruebaTecnica-app/vite.config.js
@@ -3,16 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    importToCDN({
-        modules: [
-            autoComplete('react'),
-            autoComplete('react-dom'),
-            autoComplete('moment'),
-            autoComplete('antd'),
-            autoComplete('react-bootstrap')
-        ],
-    }),
-    reactRefresh(),
-],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'react-router-dom',
+      ]
+    }
+  }
 })
